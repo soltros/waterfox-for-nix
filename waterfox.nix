805515afc9f84e8e5166d84a88a33284642dd3b7
Derrik Diener <142091676+soltros@users.noreply.github.com>
2025-05-27 23:36:08 -0400

    Update waterfox.nix

--- waterfox.nix
+++ waterfox.nix
@@ -1,12 +1,19 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
+##############################################################################
+# NixOS module: programs.waterfox
+##############################################################################
 let
+  # Re-use your existing derivation
   waterfox = pkgs.callPackage ./default.nix { };
 in
 with lib;
 {
-  options.programs.waterfox.enable = mkEnableOption "the Waterfox web browser";
+  ###### 1. Option ##########################################################
+  options.programs.waterfox.enable =
+    mkEnableOption "the Waterfox binary web browser";
 
+  ###### 2. Implementation ##################################################
   config = mkIf config.programs.waterfox.enable {
     environment.systemPackages = [ waterfox ];
   };
