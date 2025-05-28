c37eb47fb854bb4b6a136e513e75309c027151e9
Derrik Diener <142091676+soltros@users.noreply.github.com>
2025-05-27 23:58:30 -0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -11,27 +11,35 @@ pkgs.stdenv.mkDerivation rec {
     sha256 = "0oouh44thdir6mo4o2gtno58th946ktapdoe88l2c63i0fs0ddnb";
   };
 
-  nativeBuildInputs = with pkgs; [ autoPatchelfHook copyDesktopItems wrapGAppsHook ];
-  buildInputs       = with pkgs; [ alsa-lib dbus-glib gtk3 stdenv.cc.cc.lib xorg.libXtst libva pciutils curl ];
-  runtimeDependencies= with pkgs; [ curl libva.out pciutils ];
-  appendRunpaths    = [ (lib.getLib pkgs.pipewire) ];
+  nativeBuildInputs    = with pkgs; [ autoPatchelfHook copyDesktopItems wrapGAppsHook ];
+  buildInputs          = with pkgs; [ alsa-lib dbus-glib gtk3 stdenv.cc.cc.lib xorg.libXtst libva pciutils curl ];
+  runtimeDependencies  = with pkgs; [ curl libva.out pciutils ];
+  appendRunpaths       = [ (lib.getLib pkgs.pipewire) ];
 
   ## phases ---------------------------------------------------------------
-  unpackPhase   = ''runHook preUnpack; tar -xf $src; runHook postUnpack'';
+  unpackPhase   = ''
+    runHook preUnpack
+    tar -xf $src
+    runHook postUnpack
+  '';
   dontConfigure = true;
   dontBuild     = true;
 
   installPhase = ''
     runHook preInstall
     mkdir -p $out/{bin,lib/waterfox,share/icons/hicolor/128x128/apps}
-    cp -r * $out/lib/waterfox/
 
-    cat > $out/bin/waterfox <<EOF
+    # copy *contents* of the directory, not the directory itself
+    cp -r waterfox/* $out/lib/waterfox/
+
+    # wrapper
+    cat > $out/bin/waterfox <<'EOF'
 #!/bin/sh
-exec $out/lib/waterfox/waterfox "\$@"
+exec "$(dirname "$0")/../lib/waterfox/waterfox" "$@"
 EOF
     chmod +x $out/bin/waterfox
 
+    # icon
     if [ -f $out/lib/waterfox/browser/chrome/icons/default/default128.png ]; then
       cp $out/lib/waterfox/browser/chrome/icons/default/default128.png \
          $out/share/icons/hicolor/128x128/apps/waterfox.png
@@ -41,26 +49,26 @@ EOF
 
   desktopItems = [
     (pkgs.makeDesktopItem {
-      name        = "waterfox";
-      desktopName = "Waterfox";
-      genericName = "Web Browser";
-      exec        = "waterfox %U";
-      icon        = "waterfox";
-      mimeTypes   = [
+      name             = "waterfox";
+      desktopName      = "Waterfox";
+      genericName      = "Web Browser";
+      exec             = "waterfox %U";
+      icon             = "waterfox";
+      mimeTypes        = [
         "application/vnd.mozilla.xul+xml"
         "application/xhtml+xml"
         "text/html" "text/xml"
         "x-scheme-handler/http"
         "x-scheme-handler/https"
       ];
-      categories    = [ "Network" "WebBrowser" ];
-      startupNotify = true;
-      startupWMClass= "waterfox";
-      terminal      = false;
+      categories       = [ "Network" "WebBrowser" ];
+      startupNotify    = true;
+      startupWMClass   = "waterfox";
+      terminal         = false;
       actions = {
-        "new-window"      = { name = "New Window";       exec = "waterfox --new-window %U"; };
-        "private-window"  = { name = "New Private Window"; exec = "waterfox --private-window %U"; };
-        "profile-manager" = { name = "Profile Manager";  exec = "waterfox --ProfileManager"; };
+        "new-window"      = { name = "New Window";        exec = "waterfox --new-window %U"; };
+        "private-window"  = { name = "New Private Window";exec = "waterfox --private-window %U"; };
+        "profile-manager" = { name = "Profile Manager";   exec = "waterfox --ProfileManager"; };
       };
     })
   ];
